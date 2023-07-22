d112fd7c2bd9648b043cb3a2ae8590c7bd3acfd3
George <gwwebster23@gmail.com>
2023-07-21 22:22:41 -0500

    Fibonacci sequence

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length == 0:
+        fib_seq = list()
+        print(fib_seq)
+    elif length == 1:
+        fib_seq = list([0])
+        print(fib_seq)
+    elif length > 1:
+        fib_seq = list([0, 1])
+        for int in range(length - 2): 
+            fib_seq.append(fib_seq[-1] + fib_seq[-2])
+        print(fib_seq)
